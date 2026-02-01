3430cb5ac660e98a4bccd93ed3fae2c790b3c7cb
MOHD ADEEB <142772846+Dewolf1@users.noreply.github.com>
2026-02-01 14:13:50 +0530

    Swap Head entries

--- src/data/team.js
+++ src/data/team.js
@@ -46,17 +46,18 @@ export const teamMembers = [
         }
     },
     {
-        id: 'mgmt-head',
-        name: 'Satyam Raj',
-        role: 'Management Head',
+        id: 'tech-head',
+        name: 'Mohd Adeeb',
+        role: 'Technical Head',
         year: '3rd Year',
-        image: satyamImg,
+        image: adeebImg,
         socials: {
             github: '#',
             linkedin: '#',
             instagram: '#'
         }
     },
+ 
     {
         id: 'pr-head',
         name: 'Harshit Pathak',
@@ -81,17 +82,16 @@ export const teamMembers = [
             instagram: '#'
         }
     },
-    {
-        id: 'tech-head',
-        name: 'Mohd Adeeb',
-        role: 'Technical Head',
-        year: '3rd Year',
-        image: adeebImg,
-        socials: {
+    { 
+        id: 'mgmt-head',
+        name: 'Satyam Raj',
+        role: 'Management Head', year: '3rd Year',
+        image: satyamImg,
+        socials: { 
             github: '#',
             linkedin: '#',
             instagram: '#'
-        }
+         }
     },
     {
         id: 'mkting-head',
